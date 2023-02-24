bb4b7b568354ed13c11f2c80d2db81b8f14c4bac
Rob Spectre <rob@brooklynhacker.com>
2023-02-24 11:49:18 -0500

    1) Adjusted `sys.path` for RTD.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../.."))
 
 extensions = ['sphinx.ext.autodoc']
 
